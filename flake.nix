41c0f4415e1eea69418fcdf89a11c0eae2e34ac3
Kacper Wyczawski <kacper@wyczawski.dev>
2024-06-05 16:37:40 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Kacper Wyczawski's website";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { nixpkgs, ... }:
+    let system = "x86_64-linux";
+    in {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt;
+      devShells.${system}.default = let pkgs = nixpkgs.legacyPackages.${system};
+      in pkgs.mkShell {
+        packages = with pkgs; [ bun nushell ];
+        shellHook = ''
+          exec nu
+        '';
+      };
+    };
+}
