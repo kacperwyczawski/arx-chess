b3ac820daad2a88e331c331e64118d9ebb560fea
Kacper Wyczawski <kacper@wyczawski.dev>
2024-06-09 16:39:46 +0200

    feat: refactor HTML

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import "./style.css";
 import { Board } from "./board.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-    <table id="board"></table>
-`;
-
 const board = new Board();
 board.setup(document.querySelector<HTMLTableElement>("#board")!);
