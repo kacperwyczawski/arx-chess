c384b5974fe82f0791b8e2698f99488b87668d70
Kacper Wyczawski <kacper@wyczawski.dev>
2024-07-15 17:38:50 +0200

    fix: paths in production

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        1: resolve(__dirname, 'index.html'),
+        2: resolve(__dirname, 'play.html'),
+      },
+    },
+  },
+})
