26c116877bd1c833d1f00c39a08a9808a4f87c28
Kacper Wyczawski <kacper@wyczawski.dev>
2024-08-05 13:53:49 +0200

    feat: move validation and hints for bishop

--- src/pieces/bishop.ts
+++ src/pieces/bishop.ts
@@ -1,3 +1,4 @@
+import { Cell } from "../cell";
 import { Pawn } from "./pawn";
 import type { Piece } from "./piece";
 
@@ -22,5 +23,48 @@ export class Bishop implements Piece {
 
 	constructor(color: PlayerColor) {
 		this.#color = color;
+	}
+
+	highlightMoves(cells: Cell[][], x: number, y: number): void {
+		for (let yi = y - 1, xi = x - 1; yi >= 0 && xi >= 0; yi--, xi--) {
+			const cell = cells[yi][xi]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
+		for (let yi = y + 1, xi = x - 1; yi < cells.length && xi >= 0; yi++, xi--) {
+			const cell = cells[yi][xi]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
+		for (let yi = y + 1, xi = x + 1; yi < cells.length && xi < cells.length; yi++, xi++) {
+			const cell = cells[yi][xi]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
+		for (let yi = y - 1, xi = x + 1; yi >= 0 && xi < cells.length; yi--, xi++) {
+			const cell = cells[yi][xi]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
 	}
 }
