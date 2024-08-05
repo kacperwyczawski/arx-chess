371c0500c804e118f4f5cf6bf471de347caf52de
Kacper Wyczawski <kacper@wyczawski.dev>
2024-08-05 13:45:35 +0200

    feat: move validation and hints for rook

--- src/pieces/rook.ts
+++ src/pieces/rook.ts
@@ -1,3 +1,4 @@
+import { Cell } from "../cell";
 import { Bishop } from "./bishop";
 import { Knight } from "./knight";
 import type { Piece } from "./piece";
@@ -23,5 +24,48 @@ export class Rook implements Piece {
 
 	constructor(color: PlayerColor) {
 		this.#color = color;
+	}
+
+	highlightMoves(cells: Cell[][], x: number, y: number): void {
+		for (let yi = y - 1; yi >= 0; yi--) {
+			const cell = cells[yi][x]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
+		for (let yi = y + 1; yi < cells.length; yi++) {
+			const cell = cells[yi][x]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
+		for (let xi = x - 1; xi >= 0; xi--) {
+			const cell = cells[y][xi]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
+		for (let xi = x + 1; xi < cells.length; xi++) {
+			const cell = cells[y][xi]
+			if (cell.building === "wall" || cell.piece?.color === this.#color) {
+				break;
+			}
+			cell.highlight()
+			if (cell.piece) {
+				break;
+			}
+		}
 	}
 }
